aecd378f277ae21d2bd42d2be061f0802b54da3b
Ganeshpathivada <pathivadaganesh12@gmail.com>
2022-11-10 14:03:36 +0530

    Completed UI elements for Team Page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,11 +1,15 @@
-
 import './App.css';
+import { BrowserRouter,Routes, Route} from "react-router-dom";
 import { TeamPage } from './pages/TeamPage';
 
 function App() {
   return (
     <div className="App">
-     <TeamPage />
+    <BrowserRouter>
+    <Routes>
+    <Route path="/teams/:teamName" element= {<TeamPage />} />
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -1,9 +1,19 @@
-import { React } from 'react';
+import { React } from "react";
+import { Link } from "react-router-dom";
 
-export const  MatchSmallCard = ({match}) => {
+export const MatchSmallCard = ({ match, teamName }) => {
+  if (!match) return null;
+  const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
+
+  const otherTeamRoute = `/teams/${otherTeam}`;
   return (
     <div className="MatchSmallCard">
-    <p>{match.team1} vs {match.team2}</p>
+      <h3>
+        vs <Link to={otherTeamRoute}>{otherTeam}</Link>
+      </h3>
+      <h3>
+        {match.matchWinner} won by {match.resultMargin} {match.result}
+      </h3>
     </div>
   );
-}
+};
