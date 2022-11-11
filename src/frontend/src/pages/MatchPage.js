2e875c8e6499789a3bd1cc1465837c5f878b77e1
Ganeshpathivada <pathivadaganesh12@gmail.com>
2022-11-11 07:29:56 +0530

    Updated Match Page

---
+++ src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,34 @@
+import { React ,useState , useEffect} from 'react'
+import { useParams } from 'react-router-dom';
+
+import { MatchDetailCard } from '../components/MatchDetailCard';
+
+export const MatchPage = () => {
+    const [matches , setMatches] = useState();
+    const { teamName,year } = useParams();
+
+    useEffect(
+        () =>{
+            const fetchMatches = async () => {
+                const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+                const data = await response.json();
+                setMatches(data);
+            };
+            fetchMatches();
+        },[]
+    );
+
+    if(!matches ){
+        return <h1>Match not found</h1>; 
+      }
+
+
+    return (
+        <div className='MatchPage'>
+        <h1>Match Page</h1>{
+          matches.map(match => <MatchDetailCard teamName={teamName} match={match}/>)
+        }
+        </div>
+
+    );
+}
